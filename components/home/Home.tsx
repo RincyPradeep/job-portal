f98055cf3c284375296f372e646638ce3d7b6f51
Rincy Pradeep <rincypradeep15@gmail.com>
2026-01-05 11:52:57 +0530

    Category section

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './hero/Hero'
+import Category from './category/Category'
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Hero />
+      <Category />
     </div>
   )
 }
