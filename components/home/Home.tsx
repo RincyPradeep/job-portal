320fba21af6710db19db4bb38ee3a2afe46b428c
Rincy Pradeep <rincypradeep15@gmail.com>
2026-01-06 13:15:26 +0530

    Company section slider

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './hero/Hero'
 import Category from './category/Category'
+import Job from './job/Job'
+import TopCompany from './top_company/TopCompany'
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Hero />
       <Category />
+      <Job />
+      <TopCompany />
     </div>
   )
 }
